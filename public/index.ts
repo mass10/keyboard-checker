d31b0aa70d5dea4fb7dd38acc49056772ab126f3
Irisawa Masaru <mass10.github@gmail.com>
2020-04-21 21:22:04 +0900

    wip

--- public/index.ts
+++ public/index.ts
@@ -1,12 +1,13 @@
 namespace Index {
 
-	function onLoad(): void {
-		console.log("[TRACE] <onLoad()>");
+	function onLoadWindow(): void {
+		console.log("[TRACE] <onLoadWindow()>");
+		alert("load!");
 	}
 
 	export function main(): void {
 		console.log("[TRACE] ### start ###");
-		window.addEventListener("load", onLoad);
+		window.addEventListener("load", onLoadWindow);
 		console.log("[TRACE] --- end ---");
 	}
 }
