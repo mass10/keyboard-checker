e4a58fd672b79e6cea89abcc4a153500862c8eb4
Irisawa Masaru <mass10.github@gmail.com>
2020-04-21 21:44:51 +0900

    wip

--- public/index.ts
+++ public/index.ts
@@ -2,12 +2,93 @@ namespace Index {
 
 	function onLoadWindow(): void {
 		console.log("[TRACE] <onLoadWindow()>");
-		alert("load!");
+	}
+
+	function convertKey(e: KeyboardEvent): string {
+		if (e.code == "Digit1") return "1";
+		if (e.code == "Digit2") return "2";
+		if (e.code == "Digit3") return "3";
+		if (e.code == "Digit4") return "4";
+		if (e.code == "Digit5") return "5";
+		if (e.code == "Digit6") return "6";
+		if (e.code == "Digit7") return "7";
+		if (e.code == "Digit8") return "8";
+		if (e.code == "Digit9") return "9";
+		if (e.code == "Digit0") return "0";
+		if (e.code == "Minus") return "-";
+		if (e.code == "Equal") return "^";
+		if (e.code == "IntlYen") return "IntlYen";
+		if (e.code == "KeyQ") return "Q";
+		if (e.code == "KeyW") return "W";
+		if (e.code == "KeyE") return "E";
+		if (e.code == "KeyR") return "R";
+		if (e.code == "KeyT") return "T";
+		if (e.code == "KeyY") return "Y";
+		if (e.code == "KeyU") return "U";
+		if (e.code == "KeyI") return "I";
+		if (e.code == "KeyO") return "O";
+		if (e.code == "KeyP") return "P";
+		if (e.code == "BracketLeft") return "@";
+		if (e.code == "BracketRight") return "[";
+		if (e.code == "Enter") return "Enter";
+		if (e.code == "KeyA") return "A";
+		if (e.code == "KeyS") return "S";
+		if (e.code == "KeyD") return "D";
+		if (e.code == "KeyF") return "F";
+		if (e.code == "KeyG") return "G";
+		if (e.code == "KeyH") return "H";
+		if (e.code == "KeyJ") return "J";
+		if (e.code == "KeyK") return "K";
+		if (e.code == "KeyL") return "L";
+		if (e.code == "Semicolon") return ";";
+		if (e.code == "Quote") return ":";
+		if (e.code == "Backslash") return "]";
+		if (e.code == "KeyZ") return "Z";
+		if (e.code == "KeyX") return "X";
+		if (e.code == "KeyC") return "C";
+		if (e.code == "KeyV") return "V";
+		if (e.code == "KeyB") return "B";
+		if (e.code == "KeyN") return "N";
+		if (e.code == "KeyM") return "M";
+		if (e.code == "Comma") return ",";
+		if (e.code == "Period") return ".";
+		if (e.code == "Slash") return "/";
+		if (e.code == "IntlRo") return "IntlRo";
+		if (e.code == "Space") return "Space";
+		return "";
+	}
+
+	function onKeyDown(e: KeyboardEvent) {
+		const keycode = convertKey(e);
+		if (keycode === "") {
+			console.log("[ERROR] キーを判別できません。", e);
+			return;
+		}
+		const element = document.getElementById(keycode);
+		if (!element) {
+			return;
+		}
+		element.style.borderColor = "#e06060";
+	}
+
+	function onKeyUp(e: KeyboardEvent) {
+		const keycode = convertKey(e);
+		if (keycode === "") {
+			console.log("[ERROR] キーを判別できません。", e);
+			return;
+		}
+		const element = document.getElementById(keycode);
+		if (!element) {
+			return;
+		}
+		element.style.borderColor = "#b0b0b0";
 	}
 
 	export function main(): void {
 		console.log("[TRACE] ### start ###");
 		window.addEventListener("load", onLoadWindow);
+		document.addEventListener("keydown", onKeyDown);
+		document.addEventListener("keyup", onKeyUp);
 		console.log("[TRACE] --- end ---");
 	}
 }
